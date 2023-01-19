1bc9c29c908a400fd45ff1816e3de64c312d72a0
Peter Sutter <peter.sutter@sap.com>
2023-01-19 17:07:43 +0100

    bump golangci-lint to v1.50.1

--- hack/golangci-lint.sh
+++ hack/golangci-lint.sh
@@ -19,7 +19,7 @@ fi
 GOLANGCI_LINT_ADDITIONAL_FLAGS=${GOLANGCI_LINT_ADDITIONAL_FLAGS:-""}
 
 # Install golangci-lint (linting tool)
-curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.46.2
+curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.50.1
 
 echo "> Lint gardenlogin"
 
