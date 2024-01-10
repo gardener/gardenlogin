58c05a10b43122bae2d2758d7a5f9b5afb619fbe
vasu1124 <vasu1124@actvirtual.com>
2024-01-10 13:57:07 +0100

    add spdx license header

--- flake.nix
+++ flake.nix
@@ -1,3 +1,8 @@
+/*
+SPDX-FileCopyrightText: 2021 SAP SE or an SAP affiliate company and Gardener contributors
+
+SPDX-License-Identifier: Apache-2.0
+*/
 {
   description = "Nix flake for gardenlogin";
 
