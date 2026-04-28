43ab0a88ad0d5dd3d5da4802238648a4eb7d2141
gardener-ci-robot <gardener.ci.robot@gmail.com>
2026-04-28 07:37:17 +0000

    Update dependency securego/gosec to v2.26.1

--- hack/tools.mk
+++ hack/tools.mk
@@ -11,7 +11,7 @@ GOSEC                      := $(TOOLS_BIN_DIR)/gosec
 
 # default tool versions
 # renovate: datasource=github-releases depName=securego/gosec
-GOSEC_VERSION ?= v2.25.0
+GOSEC_VERSION ?= v2.26.1
 
 export TOOLS_BIN_DIR := $(TOOLS_BIN_DIR)
 export PATH := $(abspath $(TOOLS_BIN_DIR)):$(PATH)
